c9da8505e89e1369f082d8258742ed67a2992c2f
keke zhang <61934485+chitanderu@users.noreply.github.com>
2023-06-13 01:13:18 +0800

    Add files via upload

---
+++ music.js
@@ -0,0 +1,10 @@
+<script src="https://cdn.jsdelivr.net/npm/aplayer@1.10.1/dist/APlayer.min.js"></script>
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    audio: [{
+        name: '君に吹く風',
+        artist: '下川美娜',
+        url: '君に吹く風.mp3',
+        cover: 'https://pic.imgdb.cn/item/64395b5c0d2dde577725723e.jpg '
+    }]
+});
